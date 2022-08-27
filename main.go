04f3da411df22aac9693caf1448703cec67a397d
w3226 <106014510+w3226@users.noreply.github.com>
2022-08-28 02:57:03 +0800

    wang

--- main.go
+++ main.go
@@ -174,7 +174,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "填入默认机器人的名称", "Set default nickname.")
+	adana := flag.String("n", "樱樱", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -204,7 +204,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus,写上你的QQ号 )
+	// sus = append(sus,3226558142 )
 	// sus = append(sus, 87654321)
 
 	// 启用 gui
